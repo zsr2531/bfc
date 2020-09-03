41f7ba8848d3d5c552af84965159c2f64b2674c2
zsr2531 <warhaamer1337@gmail.com>
2020-09-03 08:31:39 +0200

    Pretty print them Ast bois!

---
+++ utils.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "ast.hpp"
+#include <iostream>
+#include <locale>
+#include <codecvt>
+
+// Thank you Windows for being a pile of shit and having to need this workaround (:
+inline static auto charFromValue(int value, int count = 1) -> std::string {
+    return std::string(count, value);
+}
+
+auto prettyPrint(const std::unique_ptr<Statement>& statement, std::string indent, bool isLast = true) -> void {
+    static std::unordered_map<StatementKind, std::string> kindToText = {
+        { StatementKind::Print, "Print" },
+        { StatementKind::Input, "Input" },
+        { StatementKind::ShiftLeft, "ShiftLeft" },
+        { StatementKind::ShiftRight, "ShiftRight" },
+        { StatementKind::Loop, "Loop" },
+        { StatementKind::Increment, "Increment" },
+        { StatementKind::Decrement, "Decrement" }
+    };
+
+    static auto twoLines = charFromValue(196, 2);
+    static auto corner = charFromValue(192);
+    static auto middleLine = charFromValue(195);
+    static auto verticalLine = charFromValue(179);
+    static auto lastEdge = corner + twoLines;
+    static auto notLastEdge = middleLine + twoLines;
+
+    auto marker = isLast ? lastEdge : notLastEdge;
+    std::cout << indent;
+    std::cout << marker;
+    std::cout << kindToText[statement->kind()] << std::endl;
+
+    indent += isLast ? "    " : verticalLine + "   ";
+
+    if (statement->kind() != StatementKind::Loop)
+        return;
+    
+    auto& loop = dynamic_cast<LoopStatement&>(*statement);
+    auto& children = loop.statements;
+    if (children.empty())
+        return;
+
+    auto& last = children.back();
+    for (auto& child : children)
+        prettyPrint(child, indent, child == last);
+}
