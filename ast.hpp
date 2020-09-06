b4e802272d3d14e594328b30ca1a372a7296a9b5
zsr2531 <warhaamer1337@gmail.com>
2020-09-06 12:04:55 +0200

    Add listener contract and expand ast nodes

--- ast.hpp
+++ ast.hpp
@@ -36,19 +36,27 @@ public:
 
 class ShiftLeftStatement : public Statement {
 public:
+    long long by;
+
+    explicit ShiftLeftStatement(long long by = 1) : by(by) { }
+
     auto kind() const -> StatementKind override { return StatementKind::ShiftLeft; }
     auto accept(Visitor& visitor) -> void override;
 };
 
 class ShiftRightStatement : public Statement {
 public:
+    long long by;
+
+    explicit ShiftRightStatement(long long by = 1) : by(by) { }
+
     auto kind() const -> StatementKind override { return StatementKind::ShiftRight; }
     auto accept(Visitor& visitor) -> void override;
 };
 
 class LoopStatement : public Statement {
 public:
-    const std::vector<std::unique_ptr<Statement>> statements;
+    std::vector<std::unique_ptr<Statement>> statements;
 
     auto kind() const -> StatementKind override { return StatementKind::Loop; }
     auto accept(Visitor& visitor) -> void override;
@@ -58,12 +66,20 @@ public:
 
 class IncrementStatement : public Statement {
 public:
+    long long by;
+
+    explicit IncrementStatement(long long by = 1) : by(by) { }
+
     auto kind() const -> StatementKind override { return StatementKind::Increment; }
     auto accept(Visitor& visitor) -> void override;
 };
 
 class DecrementStatement : public Statement {
 public:
+    long long by;
+
+    explicit DecrementStatement(long long by = 1) : by(by) { }
+
     auto kind() const -> StatementKind override { return StatementKind::Decrement; }
     auto accept(Visitor& visitor) -> void override;
 };
@@ -77,4 +93,32 @@ public:
     virtual auto visit(const LoopStatement& loopStatement) -> void { };
     virtual auto visit(const IncrementStatement& incrementStatement) -> void { };
     virtual auto visit(const DecrementStatement& decrementStatement) -> void { };
+};
+
+class Listener : public Visitor {
+protected:
+    virtual auto visitPrintStatement(const PrintStatement& printStatement) -> void { };
+    virtual auto visitInputStatement(const InputStatement& inputStatement) -> void { };
+    virtual auto visitShiftLeftStatement(const ShiftLeftStatement& shiftLeftStatement) -> void { };
+    virtual auto visitShiftRightStatement(const ShiftRightStatement& shiftRightStatement) -> void { };
+    virtual auto visitIncrementStatement(const IncrementStatement& incrementStatement) -> void { };
+    virtual auto visitDecrementStatement(const DecrementStatement& decrementStatement) -> void { };
+    virtual auto enterLoopStatement(const LoopStatement& loopStatement) -> void { };
+    virtual auto exitLoopStatement(const LoopStatement& loopStatement) -> void { };
+private:
+    auto visit(const PrintStatement& printStatement) -> void override { visitPrintStatement(printStatement); };
+    auto visit(const InputStatement& inputStatement) -> void override { visitInputStatement(inputStatement); };
+    auto visit(const ShiftLeftStatement& shiftLeftStatement) -> void override { visitShiftLeftStatement(shiftLeftStatement); };
+    auto visit(const ShiftRightStatement& shiftRightStatement) -> void override { visitShiftRightStatement(shiftRightStatement); };
+    auto visit(const IncrementStatement& incrementStatement) -> void override { visitIncrementStatement(incrementStatement); };
+    auto visit(const DecrementStatement& decrementStatement) -> void override { visitDecrementStatement(decrementStatement); };
+    auto visit(const LoopStatement& loopStatement) -> void override {
+        enterLoopStatement(loopStatement);
+
+        for (auto& statement : loopStatement.statements) {
+            statement->accept(*this);
+        }
+
+        exitLoopStatement(loopStatement);
+    }
 };
