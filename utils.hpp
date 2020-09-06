c01839db602728235715b8f42dc7be60d7834ac8
zsr2531 <warhaamer1337@gmail.com>
2020-09-06 12:07:18 +0200

    Fuck extended ascii too

--- utils.hpp
+++ utils.hpp
@@ -5,12 +5,7 @@
 #include <locale>
 #include <codecvt>
 
-// Thank you Windows for being a pile of shit and having to need this workaround (:
-inline static auto charFromValue(int value, int count = 1) -> std::string {
-    return std::string(count, value);
-}
-
-auto prettyPrintStatement(const std::unique_ptr<Statement>& statement, std::string indent, bool isLast = true) -> void {
+auto prettyPrintStatement(const std::unique_ptr<Statement>& statement, std::string indent) -> void {
     static std::unordered_map<StatementKind, std::string> kindToText = {
         { StatementKind::Print, "Print" },
         { StatementKind::Input, "Input" },
@@ -21,19 +16,10 @@ auto prettyPrintStatement(const std::unique_ptr<Statement>& statement, std::stri
         { StatementKind::Decrement, "Decrement" }
     };
 
-    static auto twoLines = charFromValue(196, 2);
-    static auto corner = charFromValue(192);
-    static auto middleLine = charFromValue(195);
-    static auto verticalLine = charFromValue(179);
-    static auto lastEdge = corner + twoLines;
-    static auto notLastEdge = middleLine + twoLines;
-
-    auto marker = isLast ? lastEdge : notLastEdge;
     std::cout << indent;
-    std::cout << marker;
     std::cout << kindToText[statement->kind()] << std::endl;
 
-    indent += isLast ? "    " : verticalLine + "   ";
+    indent += "  ";
 
     if (statement->kind() != StatementKind::Loop)
         return;
@@ -43,7 +29,6 @@ auto prettyPrintStatement(const std::unique_ptr<Statement>& statement, std::stri
     if (children.empty())
         return;
 
-    auto& last = children.back();
     for (auto& child : children)
-        prettyPrintStatement(child, indent, child == last);
+        prettyPrintStatement(child, indent);
 }
